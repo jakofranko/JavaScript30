dfad50e484d01f6efb63318dabe62dc63d0a70a9
Jake Franklin <jacob.d.franklin@gmail.com>
2017-08-19 13:21:03 -0700

    Fixed a bad event name

--- 20/js/main.js
+++ 20/js/main.js
@@ -7,7 +7,7 @@ let p = document.createElement('p');
 const words = document.querySelector('.words');
 words.appendChild(p);
 
-recognition.addEventListener('results', e => {
+recognition.addEventListener('result', e => {
 console.log(e);
 const transcript = Array.from(e.results)
   .map(result => result[0])

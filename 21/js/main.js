6b8b5f088fba3146e3bbc0478ca6a02828d168e1
Jake Franklin <jacob.d.franklin@gmail.com>
2017-08-19 22:16:01 -0700

    Added some text to indicate updates

--- 21/js/main.js
+++ 21/js/main.js
@@ -1,11 +1,13 @@
 const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
+const update = document.querySelector('.update');
 
 navigator.geolocation.watchPosition((data) => {
-console.log(data);
-speed.textContent = data.coords.speed;
-arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+    update.style.opacity = Number(update.style.opacity) ? 0 : 1;
 }, (err) => {
-console.error(err);
-alert('The compass will no worky till you allow that');
+    console.error(err);
+    alert('The compass will no worky till you allow that');
 });
